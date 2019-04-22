9f52dc9238ff92f1c42d49f367d32f68b8feb518
Alicizations <673386754@qq.com>
2019-04-22 19:44:09 +0800

    account v1.0

--- imServer/account/models.py
+++ imServer/account/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    UserID = models.AutoField(primary_key = True)
+    Username = models.CharField(max_length = 20)
+    Password = models.CharField(max_length = 20)
+    Phone = models.CharField(max_length = 15)
+    Email = models.CharField(max_length = 30)
+    Nickname = models.CharField(max_length = 20)
+    Avator = models.IntegerField(default = -1)
+    Description = models.CharField(max_length = 40)
+
+    def __str__(self):
+        return self.Username
--- imServer/account/views.py
+++ imServer/account/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
+import json
+from .models import User
 
 # Create your views here.
+
+def register(request):
+
+    if request.method != 'POST':
+        return HttpResponse("register page")
+
+    response = {'state':'ok', 'msg':'register successfully'}
+
+    # 获取参数
+    try:
+        t_username = request.POST['username']
+        t_password = request.POST['password']
+    except Exception as e:
+        response = {'state':'fail', 'msg':'POST parameter error'}
+        return HttpResponse(json.dumps(response), content_type = 'application/json')
+
+    # 数据库操作
+    try:
+        t_user = User.objects.filter(Username = t_username)
+    except Exception as e:
+        response = {'state':'fail', 'msg':'db error'}
+    else:
+        if t_user.count() == 0:
+            User.objects.create(
+                Username = t_username,
+                Password = t_password
+            )
+        else:
+            response = {'state':'failed', 'msg':'repeat username'}
+
+    return HttpResponse(json.dumps(response), content_type = 'application/json')
+
+
+def login(request):
+    if request.method != 'POST':
+        return HttpResponse("login page")
+
+    response = {'state':'ok', 'msg':'login successfully'}
+
+    # 获取参数
+    try:
+        t_username = request.POST['username']
+        t_password = request.POST['password']
+    except Exception as e:
+        response = {'state':'fail', 'msg':'POST parameter error'}
+        return HttpResponse(json.dumps(response), content_type = 'application/json')
+
+    # 数据库操作
+    try:
+        t_user = User.objects.filter(Username = t_username)
+    except Exception as e:
+        response = {'state':'fail', 'msg':'db error'}
+    else:
+        if t_user.count() <= 0:
+            response = {'state':'failed', 'msg':'username or password error'}
+
+    return HttpResponse(json.dumps(response), content_type = 'application/json')
