50b65faf67433491c0657a4a8c0c33e1d8792fe5
Alicizations <673386754@qq.com>
2019-04-22 20:15:32 +0800

    ac v1.1, add get info

--- imServer/account/urls.py
+++ imServer/account/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
+    path('info/<str:t_username>/', views.info, name='info'),
 ]
--- imServer/account/views.py
+++ imServer/account/views.py
@@ -1,4 +1,5 @@
 # from django.shortcuts import render
+from django.forms.models import model_to_dict
 from django.http import HttpResponse
 import json
 from .models import User
@@ -61,3 +62,22 @@ def login(request):
             response = {'state':'failed', 'msg':'username or password error'}
 
     return HttpResponse(json.dumps(response), content_type = 'application/json')
+
+
+def info(request, t_username):
+    if request.method == 'POST':
+        return HttpResponse("fail")
+
+    response = {'state':'fail', 'msg':'no data'}
+
+    try:
+        t_user = User.objects.filter(Username = t_username)
+    except Exception as e:
+        response = {'state':'fail', 'msg':'db error'}
+    else:
+        if t_user.count() == 1:
+            temp = model_to_dict(t_user[0])
+            response = {'state':'ok', 'msg':'ok', "data":temp}
+
+    return HttpResponse(json.dumps(response), content_type = 'application/json')
+            
