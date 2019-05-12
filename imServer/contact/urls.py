87b0dc3f014b97d52feb5af8bf1ae9e542b96def
Alicizations <673386754@qq.com>
2019-05-12 14:21:49 +0800

    update

--- imServer/contact/urls.py
+++ imServer/contact/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/info', views.info, name='info'),
-    path('/add', views.add, name='add'),
-    path('/delete', views.delete, name='delete'),
+    path('info', views.info, name='info'),
+    path('add', views.add, name='add'),
+    path('delete', views.delete, name='delete'),
 ]
--- imServer/history/urls.py
+++ imServer/history/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/personal', views.personal, name='personal'),
+    path('personal', views.personal, name='personal'),
     # 群聊消息记录，暂未实现
-    path('/group', views.group, name='group'),
+    path('group', views.group, name='group'),
 ]
