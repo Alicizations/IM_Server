9e3b119a93c5194838cfa573cefcd89731d45445
Alicizations <673386754@qq.com>
2019-06-01 11:19:33 +0800

    update

--- imServer/imServer/urls.py
+++ imServer/imServer/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('content/', include('content.urls')),
     # path('history', include('history.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)  # 加上statis
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) # 加上statis
